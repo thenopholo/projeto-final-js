3abf5027ae6e90ddac6302525109e0c6454846f2
Rodrigo Thenopholo <thenopholo92@icloud.com>
2024-03-26 15:04:44 -0300

    Form de login centralizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,23 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: '#0d9488',
-                secondary: '#facc15',
+                primary: '#1F2937',
+                secondary: '#6366F1',
                 accent: '#64748b',
                 background: '#f8fafc',
-                text: '#111827',
             },
-        },
-        fontFamily: {
-            sans: ['Inter', 'sans-serif'],
-            heading: ['Poppins', 'sans-serif'],
-        },
-        spacing: {
-            '8': '2rem', // Example spacing
-            // Add more as needed
-        },
-        borderRadius: {
-            'lg': '1.5rem',
-            // Define other border radius as needed
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+                heading: ['Poppins', 'sans-serif'],
+            },
+            spacing: {
+                '8': '2rem', // Example spacing
+                // Add more as needed
+            },
+            borderRadius: {
+                'lg': '1.5rem',
+                // Define other border radius as needed
+            },
         },
     },
 }
