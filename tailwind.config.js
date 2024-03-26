997d7821155b061ea714a33ac7440bab4d0fe32c
Rodrigo Thenopholo <thenopholo92@icloud.com>
2024-03-26 14:50:30 -0300

    Projeto Inicializado

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0d9488',
+                secondary: '#facc15',
+                accent: '#64748b',
+                background: '#f8fafc',
+                text: '#111827',
+            },
+        },
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+            heading: ['Poppins', 'sans-serif'],
+        },
+        spacing: {
+            '8': '2rem', // Example spacing
+            // Add more as needed
+        },
+        borderRadius: {
+            'lg': '1.5rem',
+            // Define other border radius as needed
+        },
+    },
+}
